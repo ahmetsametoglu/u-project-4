708034e1a7639bd12a3ba9c42d292e961c50df20
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-06-08 23:29:08 +0200

    feat: middlewares implemented to store

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./store/reducers";
+import middleware from "./middleware";
 
-const store = createStore(reducer);
+const store = createStore(reducer, middleware);
 
 const app = (
   <Provider store={store}>
