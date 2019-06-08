297ac2d62ebd8ea05eee6db020f7bc584150a40c
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-06-09 00:01:28 +0200

    feat: Layout hoc created

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,10 @@
+import "./Layout.css";
+import React, { Component } from "react";
+
+class Layout extends Component {
+  render() {
+    return <main>{this.props.children}</main>;
+  }
+}
+
+export default Layout;
