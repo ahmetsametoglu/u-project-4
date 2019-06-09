209f1a7c055ce898cf1cb7486b413cd3761ed924
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-06-09 18:44:17 +0200

    feat: app.js converted to class

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import React from "react";
-import { Route } from "react-router-dom";
+import React, { Component } from "react";
+import { Route, BrowserRouter } from "react-router-dom";
 
 import "./App.css";
 import Layout from "./hoc/Layout/Layout";
 import Home from "./pages/Home";
 import LeaderBoard from "./pages/LeaderBoard";
 import NewQuestion from "./pages/NewQuestion";
+import { connect } from "react-redux";
+import * as actions from "./store/actions";
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <Route path="/new-question" component={NewQuestion} />
-        <Route path="/leader-board" component={LeaderBoard} />
-        <Route path="/" exact component={Home} />
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.handleInitialData();
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Layout>
+          <Route path="/new-question" component={NewQuestion} />
+          <Route path="/leader-board" component={LeaderBoard} />
+          <Route path="/" exact component={Home} />
+        </Layout>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    handleInitialData: () => dispatch(actions.handleInitialData())
+  };
+};
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
