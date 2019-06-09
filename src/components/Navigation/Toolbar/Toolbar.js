d02ecff722fdcfed0a12d9619fc52638f3ac2f91
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-06-09 20:55:36 +0200

    feat: UserInfo component imlemented to toolbar component

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,6 +2,7 @@ import "./Toolbar.css";
 
 import React from "react";
 import NavigationItems from "./../NavigationItems/NavigationItems";
+import UserInfo from "../UserInfo/UserInfo";
 
 const Toolbar = () => {
   return (
@@ -9,6 +10,7 @@ const Toolbar = () => {
       <nav>
         <NavigationItems />
       </nav>
+      <UserInfo />
     </header>
   );
 };
