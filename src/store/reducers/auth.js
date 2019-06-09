63871cbaf9c73ee9eabb517a0109d5a8425626a5
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-06-09 18:48:19 +0200

    feat: set_auth_user action added to reducer

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,7 +1,10 @@
-export const auth = (state = null, action) => {
-    switch (action.type) {
+import * as actionTypes from "../actions/actionTypes";
 
-        default:
-            return state;
-    }
+export const auth = (state = null, action) => {
+  switch (action.type) {
+    case actionTypes.SET_AUTH_USER:
+      return { ...state, userId: action.id };
+    default:
+      return state;
+  }
 };
