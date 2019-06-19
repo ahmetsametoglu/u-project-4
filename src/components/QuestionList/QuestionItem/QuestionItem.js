5f1653ca0902508d4a784e0087c51e87c39048a7
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-06-19 07:29:21 +0200

    feat: question item created

---
+++ src/components/QuestionList/QuestionItem/QuestionItem.js
@@ -0,0 +1,36 @@
+import "./QuestionItem.css";
+
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const QuestionItem = ({ question, owner }) => {
+  return (
+    <div className="question-item-container">
+      <div className="who-ask">{owner.name} asks:</div>
+      <div className="question">
+        <div className="owner-avatar">
+          <img
+            className="avatar"
+            src={owner.avatarURL}
+            alt={`Avatar of ${owner.name}`}
+          />
+        </div>
+        <div className="question-detail">
+          <div className="would-you-rather">Would you rather ?</div>
+          <div className="tip">... {question.optionOne.text}...</div>
+          <Link to={`/question/${question.id}`} className="view-button">
+            View Poll
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionItem;
+
+QuestionItem.propTypes = {
+  question: PropTypes.object.isRequired,
+  owner: PropTypes.object.isRequired
+};
