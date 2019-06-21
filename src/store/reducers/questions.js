7d5af77ec259a0e369ffda8e8d5664bb69997f63
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-06-21 19:33:22 +0200

    feat: SET_ANSWER_TO_QUESTION reducers created

--- src/store/reducers/questions.js
+++ src/store/reducers/questions.js
@@ -5,7 +5,33 @@ export const questions = (state = {}, action) => {
     case actionTypes.RECEIVE_QUESTIONS:
       return { ...state, ...action.questions };
 
+    case actionTypes.SET_ANSWER_TO_QUESTION:
+      console.log("reducer QUESTION state", state);
+      return setAnswerToQuestion(state, action);
     default:
       return state;
   }
+};
+
+const setAnswerToQuestion = (state, action) => {
+  const { questionId, answer, authUserId } = action;
+  console.log("question=>", questionId, answer, authUserId);
+  const question = state[questionId];
+  console.log("question=>", question);
+
+  const questions = {
+    ...state,
+    [questionId]: {
+      ...question,
+      [answer]: {
+        ...question[answer],
+        votes: [...question[answer].votes, authUserId]
+      }
+    }
+  };
+
+  return {
+    ...state,
+    ...questions
+  };
 };
--- src/store/reducers/users.js
+++ src/store/reducers/users.js
@@ -4,7 +4,27 @@ export const users = (state = {}, action) => {
   switch (action.type) {
     case actionTypes.RECEIVE_USERS:
       return { ...state, ...action.users };
+    case actionTypes.SET_ANSWER_TO_QUESTION:
+      return setAnswerToQuestion(state, action);
     default:
       return state;
   }
+};
+
+const setAnswerToQuestion = (state, action) => {
+  const users = {
+    ...state,
+    [action.authUserId]: {
+      ...state[action.authUserId],
+      answers: {
+        ...state[action.authUserId].answers,
+        [action.questionId]: action.answer
+      }
+    }
+  };
+
+  return {
+    ...state,
+    ...users
+  };
 };
