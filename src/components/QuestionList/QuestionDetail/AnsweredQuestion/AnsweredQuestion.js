57e054631806a0be6be83af0793897265c039e35
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-06-21 19:42:49 +0200

    feat: answered question compnent redesigned

--- src/components/QuestionList/QuestionDetail/AnsweredQuestion/AnsweredQuestion.js
+++ src/components/QuestionList/QuestionDetail/AnsweredQuestion/AnsweredQuestion.js
@@ -1,8 +1,27 @@
 import "./AnsweredQuestion.css";
 import React from "react";
+import OptionItem from "./OptionItem/OptionItem";
 
-function AnsweredQuestion() {
-  return <div>AnsweredQuestion</div>;
-}
+const AnsweredQuestion = ({ question }) => {
+  const optionOneVoteCount = question.optionOne.votes.length;
+  const optionTwoVoteCount = question.optionTwo.votes.length;
+  const totalVoteCount = optionOneVoteCount + optionTwoVoteCount;
+  return (
+    <div>
+      <div className="question-header">Results:</div>
+
+      <OptionItem
+        text={question.optionOne.text}
+        optionVoteCount={optionOneVoteCount}
+        totalVoteCount={totalVoteCount}
+      />
+      <OptionItem
+        text={question.optionTwo.text}
+        optionVoteCount={optionTwoVoteCount}
+        totalVoteCount={totalVoteCount}
+      />
+    </div>
+  );
+};
 
 export default AnsweredQuestion;
