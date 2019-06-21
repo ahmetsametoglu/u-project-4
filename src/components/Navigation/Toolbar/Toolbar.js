f3ca17ba9cb5d2858449ed124166bcec5a2c0470
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-06-22 01:00:25 +0200

    refactor: css class name changed

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,7 +6,7 @@ import UserInfo from "../UserInfo/UserInfo";
 
 const Toolbar = () => {
   return (
-    <header className="toolbar">
+    <header className="custom-toolbar">
       <nav>
         <NavigationItems />
       </nav>
