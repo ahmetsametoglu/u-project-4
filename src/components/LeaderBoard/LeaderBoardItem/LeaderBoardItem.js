8ae15feac79d375a2af879f5b0024938924839e8
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-06-25 12:55:45 +0200

    feat: leader board item created and implemented

---
+++ src/components/LeaderBoard/LeaderBoardItem/LeaderBoardItem.js
@@ -0,0 +1,44 @@
+import "./LeaderBoardItem.css";
+
+import React from "react";
+
+const LeaderBoardItem = ({ user }) => {
+  return (
+    <div className="leader-board-item">
+      {user.orderNumber <= 3 ? (
+        <div className="leader-board-badge">
+          <span className="order-number"> {user.orderNumber}</span>
+        </div>
+      ) : null}
+      <div className="avatar-container">
+        <img
+          className="avatar"
+          src={user.avatarURL}
+          alt={`Avatar of ${user.name}`}
+        />
+      </div>
+      <div className="vertical-hr" />
+      <div className="score-info">
+        <div className="user-name">{user.name}</div>
+        <div className="score-info-item">
+          <span> Answered questions </span>
+          <span> {Object.keys(user.answers).length}</span>
+        </div>
+        <div className="horizontal-hr" />
+        <div className="score-info-item">
+          <span> Created questions </span>
+          <span> {user.questions.length}</span>
+        </div>
+      </div>
+      <div className="vertical-hr" />
+      <div className="score-container">
+        <div className="score-header">Score</div>
+        <div className="score">
+          <span className="score-item">{user.score}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeaderBoardItem;
--- src/pages/LeaderBoard.js
+++ src/pages/LeaderBoard.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { connect } from "react-redux";
+import LeaderBoardItem from "../components/LeaderBoard/LeaderBoardItem/LeaderBoardItem";
 
-const LeaderBoard = () => {
-  return <div className="leader-board-page">TODO: Leader Board Page</div>;
+const LeaderBoard = ({ users }) => {
+  return users.map(user => <LeaderBoardItem key={user.id} user={user} />);
 };
 
 const mapStateToProps = ({ users }) => {
+  const usersWithScore = Object.values(users).map(u => {
+    const score = Object.keys(u.answers).length + u.questions.length;
+    return { ...u, score: score };
+  });
+  const orderedUsers = usersWithScore
+    .sort((a, b) => (a.score > b.score ? -1 : 1))
+    .map((u, index) => {
+      return { ...u, orderNumber: index + 1 };
+    });
+
+  console.log(orderedUsers);
+
   return {
-    users
+    users: orderedUsers
   };
 };
 
