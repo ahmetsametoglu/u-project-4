bda4209dda50c73a99bd2c904d19fdfe87ee19b4
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-06-30 22:32:46 +0200

    feat: Backdrop created

---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,9 @@
+import "./Backdrop.css";
+import React from "react";
+
+const backdrop = props =>
+  props.show ? (
+    <div className={classes.Backdrop} onClick={props.clicked} />
+  ) : null;
+
+export default backdrop;
