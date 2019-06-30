d6ee9235f8e30d4d9ee60162700415f34bca390a
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-06-30 23:09:27 +0200

    feat: new-user component implemented to login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,35 @@
 import "./Login.css";
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { connect } from "react-redux";
 import * as actions from "../../store/actions";
 import LoginUserItem from "./LoginUserItem/LoginUserItem";
+import NewUser from "../NewUser/NewUser";
 
 const Login = ({ users, login }) => {
+  const [showNewUserModal, setShowNewUserModal] = useState(false);
   return (
     <Fragment>
-      <div className="login-title">Select a user to login:</div>
+      <div className="login-title">
+        Select a user to login:
+        <button
+          className="btn-create-user"
+          onClick={() => {
+            setShowNewUserModal(true);
+          }}
+        >
+          create user
+        </button>
+      </div>
       <div className="login-user-container">
         {users.map(u => (
           <LoginUserItem key={u.id} user={u} login={login} />
         ))}
       </div>
+
+      <NewUser
+        show={showNewUserModal}
+        onClose={() => setShowNewUserModal(false)}
+      />
     </Fragment>
   );
 };
