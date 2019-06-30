274d0fb838a739b7fbec4b01fa262b63a115899d
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-07-01 00:38:22 +0200

    feat: new-user component designed and connected to redux

--- src/components/NewUser/NewUser.js
+++ src/components/NewUser/NewUser.js
@@ -1,12 +1,71 @@
 import "./NewUser.css";
 import React, { useState } from "react";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import Modal from "../UI/Modal/Modal";
+import * as actions from "../../store/actions";
 
 const NewUser = props => {
+  const [userName, setUserName] = useState("");
+  const [avatarURL, setAvatarURL] = useState("/assets/avatar-icons/1.png");
+
+  let avatars = [];
+
+  for (let index = 1; index < 16; index++) {
+    const avatarPath = `/assets/avatar-icons/${index}.png`;
+    const avatarImg = (
+      <img
+        key={index}
+        className="avatar-list-item"
+        src={avatarPath}
+        alt={index}
+        onClick={() => {
+          setAvatarURL(avatarPath);
+        }}
+      />
+    );
+
+    avatars.push(avatarImg);
+  }
+
+  const isButtonDisable = () => {
+    return userName === "";
+  };
+
+  const createUser = () => {
+    props
+      .createUser(userName, avatarURL)
+      .then(() => {
+        setUserName("");
+        props.onClose();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
     <Modal show={props.show} modalClosed={props.onClose}>
-      <div>new user</div>
+      <div className="new-user-container">
+        <div className="avatar-list">{avatars}</div>
+        <img className="avatar" src={avatarURL} alt={`Avatar of new user`} />
+        <input
+          placeholder="Enter Full Name"
+          className="new-user-name"
+          value={userName}
+          onChange={event => {
+            setUserName(event.target.value);
+          }}
+        />
+
+        <button
+          className="new-user-button"
+          disabled={isButtonDisable()}
+          onClick={createUser}
+        >
+          Create User
+        </button>
+      </div>
     </Modal>
   );
 };
@@ -16,4 +75,14 @@ NewUser.propTypes = {
   onClose: PropTypes.func.isRequired
 };
 
-export default NewUser;
+const mapDispatchToProps = dispatch => {
+  return {
+    createUser: (name, avatarPath) =>
+      dispatch(actions.handleCreateUser(name, avatarPath))
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(NewUser);
