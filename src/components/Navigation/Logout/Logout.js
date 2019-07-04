6862e5f95b1f683a02ccf5ef9055877ffefbbbd9
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-07-04 06:07:09 +0200

    refactor: components converted to function component

--- src/components/Navigation/Logout/Logout.js
+++ src/components/Navigation/Logout/Logout.js
@@ -1,13 +1,20 @@
 import "./Logout.css";
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import * as actions from "../../../store/actions";
 
-class Logout extends Component {
-  render() {
-    return <button className='logout-button' onClick={this.props.logout}>logout</button>;
-  }
-}
+const Logout = props => {
+  return (
+    <button className="logout-button" onClick={props.logout}>
+      logout
+    </button>
+  );
+};
+
+Logout.prototype = {
+  logout: PropTypes.func.isRequired
+};
 
 const mapDispatchToProps = dispatch => {
   return {
--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,24 +1,28 @@
-import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
-
 import "./NavigationItem.css";
+import React from "react";
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 
-class NavigationItem extends Component {
-  render() {
-    return (
-      <li className="navigation-item">
-        <NavLink
-          to={this.props.link}
-          exact={this.props.exact}
-          activeClassName={this.props.isLogin ? "active" : null}
-        >
-          {this.props.children}
-        </NavLink>
-      </li>
-    );
-  }
-}
+const NavigationItem = props => {
+  return (
+    <li className="navigation-item">
+      <NavLink
+        to={props.link}
+        exact={props.exact}
+        activeClassName={props.isLogin ? "active" : null}
+      >
+        {props.children}
+      </NavLink>
+    </li>
+  );
+};
+
+NavigationItem.prototype = {
+  link: PropTypes.string.isRequired,
+  exact: PropTypes.bool.isRequired,
+  isLogin: PropTypes.bool.isRequired
+};
 
 const mapToProps = store => {
   return {
